649c9cde7136c4f02635e7572d4c3e9ccd6e3bc0
Andhika Rizaldi Saputra <dikarizal62@gmail.com>
2022-02-12 21:51:39 +0700

    Update script.js
    tidy up

--- javascript/script.js
+++ javascript/script.js
@@ -2,28 +2,22 @@ window.addEventListener('load', () =>{
 
     const canvas = document.querySelector('#canvas');
     const ctx = canvas.getContext('2d');
-
     const bungkus = document.querySelector('#bungkus');
-
     // ====================== resizing
     canvas.width = window.innerWidth;
     canvas.height = window.innerHeight;
-
     // ======================== variabel
     let painting = false;
-
     // ======================== startdraw
     function starpost (e){
         painting = true;
         draw(e);
-    };
-    
+    };   
     // ======================== endpost
     function endpost (){
         painting = false;
         ctx.beginPath();
     };
-
     // ========================draw
     function draw (e){
         if(!painting) return;
@@ -35,10 +29,7 @@ window.addEventListener('load', () =>{
         ctx.stroke();
         ctx.beginPath();
         ctx.moveTo(e.clientX, e.clientY);
-        
-        
     };
-
     // =======================eventlistener
     canvas.addEventListener('mousedown', starpost);
     canvas.addEventListener('mouseup', endpost);
